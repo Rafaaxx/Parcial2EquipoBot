dc2fe55d57c1fe85dc981fee04010c5c9ac86583
Rafaaxx <rafa.navarro.2006@gmail.com>
2025-05-24 13:27:30 -0300

    a

--- src/Formulario.java
+++ src/Formulario.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
-import java.awt.*;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Formulario {
 
@@ -14,11 +15,71 @@ public class Formulario {
     private JCheckBox historiaCheckBox;
     private JCheckBox tragediaCheckBox;
     private JButton guardarLibroButton;
+    private JButton leerLibrosCargadosButton;
+    private JButton terminarButton;
+    File archivolibros=new File("Registro_de_libros");
+    List<Libro> registrodelibros=new ArrayList<>();
+
     public Formulario(){
         guardarLibroButton.addActionListener(e -> guardarArchivo());
+        leerLibrosCargadosButton.addActionListener(e -> leerLibros());
+        terminarButton.addActionListener(e ->finalizar());
     }
-    public void guardarArchivo(){
-        ArrayList<String> array=new ArrayList<>();
+    public void guardarArchivo() {
+        Libro librohecho=null;
+        //Definir array
+        ArrayList<String> array = new ArrayList<>();
+        if (romanceCheckBox.isSelected()) {
+            array.add("Romance");
+        }
+        if (historiaCheckBox.isSelected()) {
+            array.add("Historia");
+        }
+        if (cienciaFicciónCheckBox.isSelected()) {
+            array.add("Ciencia Ficción");
+        }
+        if (comediaCheckBox.isSelected()) {
+            array.add("Comedia");
+        }
+        if (tragediaCheckBox.isSelected()) {
+            array.add("Tragedia");
+        }
+        if (terrorCheckBox.isSelected()) {
+            array.add("Terror");
+        }
+
+        //Crear el nuevo libro
+        try {
+             librohecho = new Libro(textField2.getText(), Integer.parseInt(textField1.getText()), textField3.getText(), array);
+        }catch (NumberFormatException e){
+            e.getMessage();
+            System.out.println("Error: La respuesta del campo 'numero de libro' no es un numero.");
+            return;
+        }
+
+        //Crear el ObjectInputStream
+        try(ObjectInputStream ois= new ObjectInputStream(new FileInputStream(archivolibros))){
+            registrodelibros=(List<Libro>) ois.readObject();
+
+        }catch (IOException | ClassNotFoundException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        registrodelibros.add(librohecho);
+        try(ObjectOutputStream oos= new ObjectOutputStream(new FileOutputStream(archivolibros))){
+            oos.writeObject(registrodelibros);
+            System.out.println("Libro guardado.");
+        }catch (IOException e){
+            System.out.println("Error al escribir el archivo");
+            e.printStackTrace();
+        }
+
+
+    }
+    public void leerLibros(){
+
+    }
+    public void finalizar(){
 
     }
 }
---
+++ src/Libro.java
@@ -0,0 +1,48 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Libro implements Serializable {
+    private String titulo;
+    private int numerolibro;
+    private String clasificacion;
+    private ArrayList<String> temas;
+
+    public Libro(String titulo, int numerolibro, String clasificacion, ArrayList<String> temas) {
+        this.titulo = titulo;
+        this.numerolibro = numerolibro;
+        this.clasificacion = clasificacion;
+        this.temas = temas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void setClasificacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public int getNumerolibro() {
+        return numerolibro;
+    }
+
+    public void setNumerolibro(int numerolibro) {
+        this.numerolibro = numerolibro;
+    }
+
+    public ArrayList<String> getTemas() {
+        return temas;
+    }
+
+    public void setTemas(ArrayList<String> temas) {
+        this.temas = temas;
+    }
+}
